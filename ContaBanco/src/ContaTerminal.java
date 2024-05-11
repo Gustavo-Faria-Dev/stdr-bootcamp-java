d9394ca819c7de15494256a685840d19a464f8ff
Gustavo Faria <gustavo.fariams@outlook.com>
2024-05-11 15:33:12 -0300

    Desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a agencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é"
+                + agencia + ", conta " + numero + "  e seu saldo" + saldo + " já está disponível para saque");
+
     }
+
 }
